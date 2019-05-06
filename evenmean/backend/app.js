4c8a32d984d53ecc12cf96b6526bbd4aafb67e4a
Tristen Even <tristen.even@msasafety.com>
2019-05-06 15:22:33 +0200

    First DB files. Config still needed

--- evenmean/backend/app.js
+++ evenmean/backend/app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://<USERINFOANDPASS>@evencluster0-1hyjx.mongodb.net/test?retryWrites=true')
+  .then(() => {
+    console.log('Connected to database...');
+  })
+  .catch((e) => {
+    console.log('Connection failed...');
+    console.log(e);
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -14,7 +26,11 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts',(req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+    timestamp: req.body.timestamp
+  });
   console.log(post);
   res.status(201).json({
     message: 'Post added successfully!'
---
+++ evenmean/backend/models/post.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  timestamp: String
+});
+
+module.exports = mongoose.model('Post', postSchema);
